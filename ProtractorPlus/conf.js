bf473c3c3f375da17eabddb4bf229d3085b33ea5
gucl1 <gucl1@lenovo.com>
2016-08-25 17:38:36 +0800

    Update conf.js, add more configration options

--- ProtractorPlus/conf.js
+++ ProtractorPlus/conf.js
@@ -10,30 +10,53 @@ exports.config = {
     // seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['Scenarios/Google/TestDemo.js'],
 
-    // params: {
-    //     Env: "default",
-    //     Country: "US"
-    // },
-    // onPrepare: function() {
-    //     jasmine.getEnv().addReporter(reporter);
-    // },
-
     capabilities: {
-      browserName: 'chrome',
-      shardTestFiles: true,
-      maxInstances: 1
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 1,
+        'chromeOptions': {
+            prefs: {
+                'download': {
+                    'directory_upgrade': true,
+                    'default_directory': 'C:/NewFolder',
+                },
+            },
+            args: [
+                'disable-extensions'
+            ]
+        },
     },
 
     onPrepare: function() {
+        // to delete cookies
+        browser.manage().deleteAllCookies();
+        // to set up the browser size
+        browser.driver.manage().window().setSize(1600, 800);
+        // to generate a html reporter after your test
         var date = new Date();
         var today = date.getFullYear() + "" + (date.getMonth() + 1) + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds();
         jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
             takeScreenshots: false,
             filePrefix: 'Reporter-USEPP-' + browser.params.Env + '-' + today
         }));
+
+        // to generate a xml reporter after your test , before that you need to "npm install mkdirp" to your testing env
+        // var folderName = (new Date()).toString().split(' ').splice(1, 4).join(' ');
+        // var mkdirp = require('mkdirp');
+        // var newFolder = "./reports/" + folderName;
+        // require('jasmine-reporters');
+        //
+        // mkdirp(newFolder, function(err) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(newFolder, true, true));
+        //     }
+        // });
     },
 
     jasmineNodeOpts: {
-        defaultTimeoutInterval: 1000000
+        defaultTimeoutInterval: 1000000,
+        showColors: true
     }
 };
